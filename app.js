4899b28eb0cf0c94f09671b3992d4689f82d088a
flo <florent.noelfl@gmail.com>
2015-05-02 15:33:21 +0200

    Basic bot for slack

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var bot = require('./aldana');
+ 
+var app = express();
+var port = process.env.PORT || 3000;
+ 
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('images'));
+ 
+// test route
+app.get('/', function (req, res) { 
+	res.status(200).send('Hello world!') 
+});
+ 
+app.get('/old', function (req, res) { 
+	res.status(200).send('old is good') 
+});
+
+app.post('/hello', bot);
+ 
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
+ 
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
